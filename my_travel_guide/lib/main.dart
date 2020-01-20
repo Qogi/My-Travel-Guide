ecd01064ec6b922200ac7c9f4c198671c5ce7b86
Qogi <cogidevs@gmail.com>
2020-01-20 17:01:03 +0000

    Created login page

--- my_travel_guide/lib/main.dart
+++ my_travel_guide/lib/main.dart
@@ -1,104 +1,121 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MyApp());
-final databaseReference = Firestore.instance;
+main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LoginPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: CustomPaint(
+        child: _buildLoginLayout(),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    );
+  }
+
+  Widget _buildLoginLayout() {
+    return Stack(
+      children: <Widget>[
+        Background(),
+        Align(
+          alignment: Alignment.topCenter,
+          child: AppLogo(),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: _buildLoginFields(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginFields() {
+    return Container(
+      margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 80.0),
+      child: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(
+              height: 20.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            _buildGoogleSignInButton(),
+            SizedBox(
+              height: 10.0,
             ),
+            _buildContinueWithoutSignInText(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+  Widget _buildGoogleSignInButton() {
+    return RaisedButton(
+      onPressed: () {},
+      color: Colors.white,
+      child: Text(
+        'Google Sign In',
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  Widget _buildContinueWithoutSignInText() {
+    return Text(
+      'Continue without signing in',
+      textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+    );
+  }
+}
+
+class AppLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      width: 150.0,
+      height: 150.0,
+      decoration:
+      BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/logo_white.png'),
+        )
+      ),
+
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:
+      BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/sign_in_page_bg.jpg'),
+            fit: BoxFit.fill
+          )
+      ),
+
     );
   }
 }
