44f99480f7618fb5c2d0207521f77d43e5184bea
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2020-02-09 09:24:05 +0000

    Created function to check if landmark is already added to timeline

--- my_travel_guide/lib/firebase/user_timeline_landmarks.dart
+++ my_travel_guide/lib/firebase/user_timeline_landmarks.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_travel_guide/authentication/google_sign_in.dart';
@@ -25,17 +25,55 @@ List<Landmark> getData() {
   return list;
 }
 
-void addLandmarkToTimeline(String placeID, String landmarkName, String dataVisited){
-  landmarkMap["Place Name"] = landmarkName;
-  landmarkMap["Date Visited"] = dataVisited;
-  landmarkMap["ID"] = placeID;
+bool checkLandmarkAlreadyAdded(String landmarkName){
+  print(landmarkName);
+  list = getData();
+  for(int i = 0; i < list.length; i++){
+    print(list.elementAt(i).time);
+    if(list.elementAt(i).time == landmarkName){
+      return true;
+    }
+  }
 
-  databaseReference
-    .collection("VisitedPlaces")
-    .document(getUserID())
-    .collection("MyPlaces")
-    .add(landmarkMap)
-    .whenComplete(() {print("Added");});
+  return false;
+}
+
+void addLandmarkToTimeline(String placeID, String landmarkName, String dataVisited){
+
+  if(checkLandmarkAlreadyAdded(landmarkName)){
+    print("Landmark already added");
+    Fluttertoast.showToast(
+      msg: "Landmark already added",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIos: 2,
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
+      fontSize: 14.0
+    );
+  }else{
+    landmarkMap["Place Name"] = landmarkName;
+    landmarkMap["Date Visited"] = dataVisited;
+    landmarkMap["ID"] = placeID;
+
+    databaseReference
+        .collection("VisitedPlaces")
+        .document(getUserID())
+        .collection("MyPlaces")
+        .add(landmarkMap)
+        .whenComplete(() {
+      Fluttertoast.showToast(
+          msg: "Landmark already added",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 2,
+          backgroundColor: Colors.white,
+          textColor: Colors.black,
+          fontSize: 14.0
+      );
+    });
+  }
+
 }
 
 void clearTimeline(){
